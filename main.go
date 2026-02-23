37b9fb5b385c82166f63fe1a913cdd1cb16d9a1d
yuyue3 <yy18825237023@126.com>
2026-02-23 15:50:40 +0800

    Update main.go

--- main.go
+++ main.go
@@ -486,7 +486,7 @@ func (a *App) topArticles(c *gin.Context) {
 }
 
 func health(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"ok": true, "timestamp": time.Now().UTC()})
+	c.JSON(http.StatusOK, gin.H{"ok": true, "author":"yy3 ,"timestamp": time.Now().UTC()})
 }
 
 // ---------- Main ----------
@@ -533,4 +533,5 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
 
